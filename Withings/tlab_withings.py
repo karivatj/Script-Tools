d903307376c5f0fb86aa685a3808c11add8934be
Kari Vatjus-Anttila <kari.vatjusanttila@gmail.com>
2017-03-02 09:53:29 +0200

    Moved to a while loop where we poll data for a set of users

--- Withings/tlab_withings.py
+++ Withings/tlab_withings.py
@@ -14,6 +14,7 @@ import datetime
 import time
 import codecs
 import sys
+import time 
 
 #autobahn for websockets
 import autobahn
@@ -61,105 +62,109 @@ if __name__ == "__main__":
     today = datetime.date.today()
     timedelta = today - datetime.timedelta(days=30)
 
+    sleep_duration = 10
+
     try:
-        for i in range (0, len(user_ids)):
-            
-            if user_ids[i] == 0:
-                continue
+        while(True):
+            for i in range (0, len(user_ids)):
+                
+                if user_ids[i] == 0:
+                    continue
 
-            user_id = user_ids[i]
-            logging.debug("Accessing Withings API for userID: " + str(user_id))
-            credentials = WithingsCredentials(user_access_tokens[i], user_access_secret[i], options.api_key, options.api_secret, user_id)
-            client = WithingsApi(credentials)
+                user_id = user_ids[i]
+                logging.debug("Accessing Withings API for userID: " + str(user_id))
+                credentials = WithingsCredentials(user_access_tokens[i], user_access_secret[i], options.api_key, options.api_secret, user_id)
+                client = WithingsApi(credentials)
 
-            measures        = []              #list to contain measurement objects
-            limit           = 20              #get last 20 measurements
-            meastype        = [10, 9, 11, 71] #weight (1), systolic / diastolic blood pressure (10 & 9), average pulse (11), body temperature (71)              
-            activity        = None
-            sleep           = None
-            sleep_summary   = None
- 
-            #dictionaries to hold measurement data 
-            avg_heartrate   = {}
-            bodytemp        = {}
-            bp              = {}
-            steps           = {}
-            calories        = {}
+                measures        = []              #list to contain measurement objects
+                limit           = 20              #get last 20 measurements
+                meastype        = [10, 9, 11, 71] #weight (1), systolic / diastolic blood pressure (10 & 9), average pulse (11), body temperature (71)              
+                activity        = None
+                sleep           = None
+                sleep_summary   = None
+     
+                #dictionaries to hold measurement data 
+                avg_heartrate   = {}
+                bodytemp        = {}
+                bp              = {}
+                steps           = {}
+                calories        = {}
 
-            #fetch all the necessary data:
-            activity = client.get_activity(startdateymd=timedelta, enddateymd=today)            #get step & calorie statistics
-            sleep_summary = client.get_sleepsummary(startdateymd=timedelta, enddateymd=today)   #get sleep summary statistics
-            for m in meastype: measures.append(client.get_measures(limit=limit, meastype=m))    #get various of user body measurements
+                #fetch all the necessary data:
+                activity = client.get_activity(startdateymd=timedelta, enddateymd=today)            #get step & calorie statistics
+                sleep_summary = client.get_sleepsummary(startdateymd=timedelta, enddateymd=today)   #get sleep summary statistics
+                for m in meastype: measures.append(client.get_measures(limit=limit, meastype=m))    #get various of user body measurements
 
-            for record in activity['activities']:
-                date = parse(record['date'])
-                steps[date] = record['steps']
-                calories[date] = record['calories'] + record['totalcalories']
-            
-            logging.debug("Parsing last " + str(limit) + " measures (measurement type(s)=" + str(meastype) + "):")
+                for record in activity['activities']:
+                    date = parse(record['date'])
+                    steps[date] = record['steps']
+                    calories[date] = record['calories'] + record['totalcalories']
+                
+                logging.debug("Parsing last " + str(limit) + " measures (measurement type(s)=" + str(meastype) + "):")
 
-            for measure in measures:
-                index = 0
-                for group in measure:
-                    for result in group.measures:                                
-                        if(result["type"] == 11): #Average Heartrate
-                            avg_heartrate[index] = normalize_value(result['value'], result['unit'])
-                            index += 1
-                        elif(result["type"] == 71): #Body Temperature
-                            bodytemp[index] = normalize_value(result['value'], result['unit'])
-                            index += 1
-                        elif(result["type"] == 10): #Systolic BP
-                            bp[index] = []
-                            bp[index].append(normalize_value(result['value'], result['unit']))
-                            index += 1                        
-                        elif(result["type"] == 9): #Diastolic BP
-                            bp[index].append(normalize_value(result['value'], result['unit']))
-                            index += 1        
+                for measure in measures:
+                    index = 0
+                    for group in measure:
+                        for result in group.measures:  
+                            date = group.date.replace(second=0).replace(minute=0).replace(hour=0)                                                  
+                            if(result["type"] == 11): #Average Heartrate
+                                avg_heartrate[date] = normalize_value(result['value'], result['unit'])
+                            elif(result["type"] == 71): #Body Temperature
+                                bodytemp[date] = normalize_value(result['value'], result['unit'])
+                            elif(result["type"] == 10): #Systolic BP
+                                bp[date] = []
+                                bp[date].append(normalize_value(result['value'], result['unit']))
+                            elif(result["type"] == 9): #Diastolic BP
+                                bp[date].append(normalize_value(result['value'], result['unit']))
 
-            logging.debug("Done! Outputting data to CSV")
-            logging.debug("")
+                logging.debug("Done! Outputting data to CSV")
+                logging.debug("")
 
-            #write fetched data to a CSV file for further processing
-            #daily steps data
-            with codecs.open(str(user_id) + "_steps_per_day.csv", "w", "utf-8") as f:
-                f.write("Date,Steps\n")
-                for record in sorted(steps):
-                    f.write(str(record) + "," + str(steps[record]) + "\n")
+                #write fetched data to a CSV file for further processing
+                #daily steps data
+                with codecs.open(str(user_id) + "_steps_per_day.csv", "w", "utf-8") as f:
+                    f.write("Date,Steps\n")
+                    for record in sorted(steps):
+                        f.write(str(record) + "," + str(steps[record]) + "\n")
 
-            #daily calorie burn
-            with codecs.open(str(user_id) + "_calories_per_day.csv", "w", "utf-8") as f:
-                f.write("Date,Calories\n")        
-                for record in sorted(calories):
-                    f.write(str(record) + "," + str(calories[record]) + "\n")
+                #daily calorie burn
+                with codecs.open(str(user_id) + "_calories_per_day.csv", "w", "utf-8") as f:
+                    f.write("Date,Calories\n")        
+                    for record in sorted(calories):
+                        f.write(str(record) + "," + str(calories[record]) + "\n")
 
-            #daily sleep duration
-            with codecs.open(str(user_id) + "_sleep_summary.csv", "w", "utf-8") as f:
-                f.write("Date,Duration\n")
-                last_measurement = datetime.datetime.fromtimestamp(0)
-                this_measurement = None
-                for record in sleep_summary['series']:
-                    this_measurement = parse(record['date'])
-                    if last_measurement < this_measurement:
-                        f.write(record['date'] + "," + str((record['data']['deepsleepduration'] + record['data']['lightsleepduration']) / 60 / 60) + "\n")
-                    last_measurement = this_measurement
+                #daily sleep duration
+                with codecs.open(str(user_id) + "_sleep_summary.csv", "w", "utf-8") as f:
+                    f.write("Date,Duration\n")
+                    last_measurement = datetime.date.fromtimestamp(0)
+                    this_measurement = None
+                    for record in sleep_summary['series']:
+                        this_measurement = parse(record['date']).date()
+                        if last_measurement < this_measurement:
+                            f.write(record['date'] + "," + str((record['data']['deepsleepduration'] + record['data']['lightsleepduration']) / 60 / 60) + "\n")
+                        last_measurement = this_measurement
 
-            #body temperature readings
-            with codecs.open(str(user_id) + "_bodytemperature.csv", "w", "utf-8") as f:
-                f.write("Index,Temperature\n")
-                for record in sorted(bodytemp):
-                    f.write(str(record) + "," + str(bodytemp[record]) + "\n")
+                #body temperature readings
+                with codecs.open(str(user_id) + "_bodytemperature.csv", "w", "utf-8") as f:
+                    f.write("Index,Temperature\n")
+                    for record in sorted(bodytemp):
+                        f.write(str(record) + "," + str(bodytemp[record]) + "\n")
 
-            #bloodpressure readings
-            with codecs.open(str(user_id) + "_bloodpressure.csv", "w", "utf-8") as f:
-                f.write("Index,Systolic,Diastolic\n")
-                for record in sorted(bp):
-                    f.write(str(record) + "," + str(bp[record][0]) + "," + str(bp[record][1]) + "\n")
-            
-            #average heartrate
-            with codecs.open(str(user_id) + "_heartrate.csv", "w", "utf-8") as f:
-                f.write("Index,Rate\n")
-                for record in sorted(avg_heartrate):
-                    f.write(str(record) + "," + str(avg_heartrate[record]) + "\n")
+                #bloodpressure readings
+                with codecs.open(str(user_id) + "_bloodpressure.csv", "w", "utf-8") as f:
+                    f.write("Index,Systolic,Diastolic\n")
+                    for record in sorted(bp):
+                        f.write(str(record) + "," + str(bp[record][0]) + "," + str(bp[record][1]) + "\n")
+                
+                #average heartrate
+                with codecs.open(str(user_id) + "_heartrate.csv", "w", "utf-8") as f:
+                    f.write("Index,Rate\n")
+                    for record in sorted(avg_heartrate):
+                        f.write(str(record) + "," + str(avg_heartrate[record]) + "\n")
+
+            logging.debug("Sleeping for " + str(sleep_duration) + " seconds until next round.")            
+            time.sleep(sleep_duration) #sleep for 10 seconds and query stats again
+
         logging.debug("Done. Goodbye!")
     except KeyboardInterrupt:
         logging.debug("Interrupted. Goodbye!")
