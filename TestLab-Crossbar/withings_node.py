1d37d5d2f2c75adc2637af27496f6f1904ce8a5b
Kari Vatjus-Anttila <kari.vatjusanttila@gmail.com>
2017-04-19 15:45:25 +0300

    Added feature to request data meant for Intersystems HealthConnect over HTTP bridge

--- TestLab-Crossbar/withings_node.py
+++ TestLab-Crossbar/withings_node.py
@@ -155,7 +155,7 @@ class AppSession(ApplicationSession):
         def withings_energy(data_format):
             data = {}
             self.log.info("withings_energy() called. Delivering payload")
-            if data_format in "plotly": #send plotly compatible data   
+            if data_format in "plotly": #return plotly compatible data   
                 for x in energy_data:
                     data[x] = format_measurement_data_to_plotly(energy_data, x)
                 return json.dumps(data)
@@ -169,10 +169,14 @@ class AppSession(ApplicationSession):
         def withings_activity(data_format):
             data = {}
             self.log.info("withings_activity() called. Delivering payload")
-            if data_format in "plotly": #send plotly compatible data   
+            if data_format in "plotly": #return plotly compatible data   
                 for x in activity_data:
                     data[x] = format_measurement_data_to_plotly(activity_data, x)
                 return json.dumps(data)
+            if data_format in "intersystems": #return plotly compatible data   
+                for x in activity_data:
+                    data[x] = format_measurement_data_to_intersystems(activity_data, x)
+                return json.dumps(data)                
             else:
                 return json.dumps(activity_data) 
 
@@ -183,7 +187,7 @@ class AppSession(ApplicationSession):
         def withings_sleep(data_format):
             data = {}
             self.log.info("withings_sleep() called. Delivering payload")
-            if data_format in "plotly": #send plotly compatible data   
+            if data_format in "plotly": #return plotly compatible data   
                 for x in sleep_data:
                     data[x] = format_measurement_data_to_plotly(sleep_data, x)
                 return json.dumps(data)
@@ -193,14 +197,19 @@ class AppSession(ApplicationSession):
         yield self.register(withings_sleep, 'com.testlab.withings_sleep')
         self.log.info("procedure withings_sleep() registered")       
        
-        # REGISTER a procedure for remote calling. Return Blood Pressure measurements
+        # REGISTER a procedure for remote calling. Return Bloodpressure measurements
         def withings_bloodpressure(data_format):
             data = {}
             self.log.info("withings_bloodpressure() called. Delivering payload")
-            if data_format in "plotly": #send plotly compatible data   
+            if data_format in "plotly": #return plotly compatible data   
                 for x in bloodpressure_data:
                     data[x] = format_bloodpressure_data_to_plotly(bloodpressure_data, x)
-                return json.dumps(data)
+                return json.dumps(data) 
+            elif data_format in "intersystems": #return intersystems compatible data via HTTP bridge
+                for x in bloodpressure_data:
+                    data = format_bloodpressure_data_to_intersystems(bloodpressure_data, x, "bloodPressure")
+                    return json.dumps(data)                    
+                #return json.dumps(data) #Testing: return first user back                
             else:
                 return json.dumps(bloodpressure_data)
 
@@ -211,10 +220,15 @@ class AppSession(ApplicationSession):
         def withings_bodytemperature(data_format):
             data = {}
             self.log.info("withings_bodytemperature() called. Delivering payload")
-            if data_format in "plotly": #send plotly compatible data   
+            if data_format in "plotly": #return plotly compatible data   
                 for x in bodytemperature_data:
                     data[x] = format_measurement_data_to_plotly(bodytemperature_data, x)
                 return json.dumps(data)
+            elif data_format in "intersystems": #return intersystems compatible data via HTTP bridge
+                for x in bodytemperature_data:
+                    data = format_measurement_data_to_intersystems(bodytemperature_data, x, "bodyTemp")
+                    return json.dumps(data)
+                #return json.dumps(data) #Testing: return first user back                
             else:
                 return json.dumps(bodytemperature_data)
 
@@ -225,7 +239,7 @@ class AppSession(ApplicationSession):
         def withings_average_heartrate(data_format):
             data = {}
             self.log.info("withings_average_heartrate() called. Delivering payload")
-            if data_format in "plotly": #send plotly compatible data   
+            if data_format in "plotly": #return plotly compatible data   
                 for x in heartrate_data:
                     data[x] = format_measurement_data_to_plotly(heartrate_data, x)
                 return json.dumps(data)
