45236d18ddec8b6f796b4b2bdf8e3f6b5b745efc
Kari Vatjus-Anttila <kari.vatjusanttila@gmail.com>
2017-05-12 14:13:26 +0300

    Moved some authentication stuff to access tokens

--- TestLab-Crossbar/access_tokens.py
+++ TestLab-Crossbar/access_tokens.py
@@ -7,3 +7,14 @@ user_access_secret = ["6e3b31ffd209fc82206939d2d59c28751759dd023e64bec198c0b7fce
                       "357dff943d283c5f9e235cc2c3b1b305f2413d3bcc50b8812c2eb4c39",
                       "efbdeb586bcbc59d0725214d3b5d2805c18022dd2fe3d5f9c91323c3eeb34d",
                       "9e9272f2e293f8e712267de68c939f4722ee936d45f28b8024d588396c7542"]
+
+mqtt_username = 'eppMqttUser'
+mqtt_password = 'iDOvPRROJiTSI9mYQptQ'
+mqtt_broker_url = 'oystestlab.epp.edelkey.net'
+mqtt_broker_port = 41883
+
+withings_api_token  = "4d81fb1981978544dfc3fe06cd93fec9004727d14bd4d26669c2be0333529"
+withings_api_secret = "7234060961cf43bff9064c2d5c4361bfb460227470c7506bc54dd77f0070"
+
+haltian_url = 'https://tmuvee.com/wp-json/tmuvee-wot/v1/d3m0-w1ll3'
+haltian_pw = 'Basic aXNlZXNvbWV0aGluZ3M6ZG95YT8='
