5c1045411e2bce6a09f31de0a9d1bc55bf764f4f
Kari Vatjus-Anttila <kari.vatjusanttila@gmail.com>
2017-05-15 10:25:41 +0300

    MQTT message handlers implemented and should work as expected

--- TestLab-Crossbar/mqtt_msgfwd.py
+++ TestLab-Crossbar/mqtt_msgfwd.py
@@ -1,30 +1,5 @@
-###############################################################################
-#
-# Copyright (c) Crossbar.io Technologies GmbH and/or collaborators. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-###############################################################################
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 from __future__ import unicode_literals
@@ -44,32 +19,39 @@ import socket
 
 mqtt_client = mqtt.Client()
 
+#These handler functions forward received WAMP events to a external MQTT Broker
 def on_haltian_location(msg):
     print("event for 'on_haltian_location' received: {}".format(msg))
     msg = json.loads(msg)
-    userid = "12762571"
+    userid = "12762562"
     data = dict()
-    data["lat"] = msg[0]["lat"]
-    data["lon"] = msg[0]["lon"]
-    mqtt_client.publish('com/testlab/user/' + user_id + '/LOC', json.dumps(data))   
+    data["lat"] = float(msg[0]["lat"])
+    data["lon"] = float(msg[0]["lon"])
+    mqtt_client.publish('com/testlab/user/' + userid + '/LOC', json.dumps(data))   
 
 def on_withings_bp(msg):
     print("event for 'on_withings_bp' received: {}".format(msg))
-    msg = json.loads(msg)
-    userid = "0"
-    #mqtt_client.publish('com/testlab/user/' + user_id + '/bloodpressure', "data")        
+    userid = msg[0] 
+    payload = msg[2]
+    data = dict()
+    data["value"] = payload
+    mqtt_client.publish('com/testlab/user/' + userid + '/bloodpressure', json.dumps(data))        
 
 def on_withings_heartrate(msg):
     print("event for 'on_withings_heartrate' received: {}".format(msg))
-    msg = json.loads(msg)    
-    userid = "0"
-    #mqtt_client.publish('com/testlab/user/' + user_id + '/avgheartrate', "data")    
+    userid = msg[0] 
+    payload = msg[2]
+    data = dict()
+    data["value"] = payload
+    mqtt_client.publish('com/testlab/user/' + userid + '/avgheartrate', json.dumps(data))    
 
 def on_withings_bodytemp(msg):
     print("event for 'on_withings_bodytemp' received: {}".format(msg))
-    msg = json.loads(msg)    
-    userid = "0"
-    #mqtt_client.publish('com/testlab/user/' + user_id + '/bodytemp', "data")    
+    userid = msg[0] 
+    payload = msg[2]
+    data = dict()
+    data["value"] = payload
+    mqtt_client.publish('com/testlab/user/' + userid + '/bodytemp', json.dumps(data))    
 
 def on_mqtt_connect(client, userdata, flags, rc):
     print("MQTT node up")
