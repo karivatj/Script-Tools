3c2f5b63de6d57a8474999ef955ceeaa3bf5b04b
Vatjus-Anttila Kari <kari.vatjusanttila@gmail.com>
2018-03-29 10:59:37 +0300

    Separate source file for utilitis needed for running this script in headless mode

---
+++ Calendar-Infoscreen/src/HeadlessUtilities.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-import argparse
+
+import csv
+import logging
+import os
+import requests
+import sys
+import traceback
+
+def headless_load_preferences():
+	pass
+
+def headless_save_preferences():
+	pass
+
+def headless_load_calendar_configuration():
+	pass
