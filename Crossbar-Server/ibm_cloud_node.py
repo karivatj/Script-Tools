c8648a24efd0587a852d09d826d80e15caa480a5
Vatjus-Anttila Kari <kari.vatjusanttila@gmail.com>
2018-05-03 12:18:24 +0300

    Added a node that connects to IBM Cloud test server

---
+++ Crossbar-Server/ibm_cloud_node.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from twisted.internet.defer import inlineCallbacks
+from twisted.logger import Logger
+
+from autobahn.twisted.util import sleep
+from autobahn.twisted.wamp import ApplicationSession
+from access_tokens import *
+
+import time
+import json
+import datetime
+import ibmiotf.application
+import ibmiotf.device
+
+TAG = "IBM Cloud Node: "
+
+class AppSession(ApplicationSession):
+
+    log = Logger()
+
+    def messageHandler(self, event)
+        print(event.deviceId)
+        print(event.deviceType)
+        print(event.timestamp)
+        print(event.timestamp.strftime("%H:%M:%S"))
+        print(event.payload)
+
+    @inlineCallbacks
+    def onJoin(self, details):
+        self.log.info(str(TAG) + "node up")
+
+        appCli = ibmiotf.application.Client(ibm_options)
+        appCli.connect()
+        appCli.subscribeToDeviceEvents(deviceType=ibm_deviceType)
+        appCli.deviceEventCallback = self.messageHandler
+
+        while True:
+            yield sleep(0.1)
