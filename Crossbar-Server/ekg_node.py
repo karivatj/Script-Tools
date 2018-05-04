12fd6a59908ba9d06f7fbcfce40179dda5d64cdf
Vatjus-Anttila Kari <kari.vatjusanttila@gmail.com>
2018-05-04 10:57:35 +0300

    Ditched file reading bc it might be unrealiable

--- Crossbar-Server/ekg_node.py
+++ Crossbar-Server/ekg_node.py
@@ -9,21 +9,14 @@ from twisted.logger import Logger
 
 from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession
+from ecg_data import ecg_data
 
 import time
 import json
 import datetime
+import itertools
 
 TAG = "ECG Node: "
-
-#generator that yields values from file
-def ecg_gen(f):
-    while True:
-        row = f.readline()
-        if len(row) == 0:
-            f.seek(0,0)
-            row = f.readline()
-        yield row
 
 class AppSession(ApplicationSession):
 
@@ -32,13 +25,10 @@ class AppSession(ApplicationSession):
     @inlineCallbacks
     def onJoin(self, details):
         self.log.info(str(TAG) + "ECG node up")
-        counter = 0
-        with open("../ECG_data.txt", "r") as f: #simulated sample of an ECG-curve
-            value = ecg_gen(f)
-            while True:
-                try:
-                    yield self.publish('com.testlab.ecg_update', json.dumps(['12762571', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'), int(next(value))]))
-                    yield sleep(0.033)
-                except Exception as e:
-                    self.log.error(str(TAG) + "Error: {}".format(e.message))
-                    pass
+        for value in itertools.cycle(ecg_data)
+            try:
+                yield self.publish('com.testlab.ecg_update', json.dumps(['12762571', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'), int(value)]))
+                yield sleep(0.033)
+            except Exception as e:
+                self.log.error(str(TAG) + "Error: {}".format(e.message))
+                pass
